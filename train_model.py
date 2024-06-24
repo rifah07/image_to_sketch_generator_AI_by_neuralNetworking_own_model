152553b8c668e82659bf1cd362926a949a9e85c0
Rifah Sajida Deya <rifahsajida7@gmail.com>
2024-06-24 06:31:21 +0600

    trained again

--- train_model.py
+++ train_model.py
@@ -53,16 +53,25 @@ def generator(input_gen, target_gen):
         targets = next(target_gen)
         yield (inputs, targets)
 
+
+# The U-Net architecture is widely used for image segmentation tasks, where the goal is to assign 
+# a label to each pixel in the image. 
+# It consists of a contracting path (encoder)
+# and an expansive path (decoder), with skip connections 
+# that transfer information from the encoder to the decoder.
+
 # U-Net model
 def unet_model(input_size=(256, 256, 3)):
     inputs = Input(input_size)
-    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs)
+    conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(inputs) #Encoding Path (Contracting Path)
     conv1 = Conv2D(64, (3, 3), activation='relu', padding='same')(conv1)
     pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    #ReLU (Rectified Linear Activation) is a widely used activation function in neural networks.
+    #It introduces non-linearity, aiding in complex pattern recognition.
 
     conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool1)
     conv2 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #maxpooling downsampling operation
 
     conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(pool2)
     conv3 = Conv2D(256, (3, 3), activation='relu', padding='same')(conv3)
@@ -74,8 +83,10 @@ def unet_model(input_size=(256, 256, 3)):
 
     conv5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(pool4)
     conv5 = Conv2D(1024, (3, 3), activation='relu', padding='same')(conv5)
+    #Two convolutional layers with 1024 filters each.
+    #This block forms the bottleneck of the U-Net, capturing the most abstract features.
 
-    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)
+    up6 = concatenate([UpSampling2D(size=(2, 2))(conv5), conv4], axis=3)#Decoding Path (Expansive Path), Up-sampling Blocks
     conv6 = Conv2D(512, (3, 3), activation='relu', padding='same')(up6)
     conv6 = Conv2D(512, (3, 3), activation='relu', padding='same')(conv6)
 
